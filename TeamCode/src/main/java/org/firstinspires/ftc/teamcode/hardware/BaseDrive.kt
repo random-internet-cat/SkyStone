552a53c7dad59da7b3826da4f261487b9f388687
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-21 13:43:32 -0400

    Add BaseDrive class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/BaseDrive.kt
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.hardware
+
+import com.qualcomm.robotcore.hardware.DcMotor
+
+open class BaseDrive {
+    private val m_frontLeft: DcMotor
+    private val m_frontRight: DcMotor
+    private val m_backLeft: DcMotor
+    private val m_backRight: DcMotor
+
+    private val DEFAULT_DRIVE_POWER = 0.8
+
+    constructor(frontLeft: DcMotor, frontRight: DcMotor, backLeft: DcMotor, backRight: DcMotor) {
+        m_frontLeft = frontLeft
+        m_frontRight = frontRight
+        m_backLeft = backLeft
+        m_backRight = backRight
+    }
+
+    private fun forEachMotor(f: DcMotor.() -> Unit) {
+        m_frontLeft.f()
+        m_frontRight.f()
+        m_backLeft.f()
+        m_backRight.f()
+    }
+
+    fun enableEncoders() {
+        forEachMotor {
+            setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER)
+            setMode(DcMotor.RunMode.RUN_USING_ENCODER)
+        }
+    }
+
+    fun disableEncoders() {
+        forEachMotor {
+            setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+        }
+    }
+
+    fun driveForward(power: Double = DEFAULT_DRIVE_POWER) {
+        require(0 <= power && power <= 1)
+
+        forEachMotor {
+            setPower(power)
+        }
+    }
+
+    fun driveBackward(power: Double = DEFAULT_DRIVE_POWER) {
+        require(0 <= power && power <= 1)
+
+        forEachMotor {
+            setPower(-power)
+        }
+    }
+}
