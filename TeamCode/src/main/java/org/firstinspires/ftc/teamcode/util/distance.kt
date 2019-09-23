7b94e9b6f58fea84e3a665299ddea464519730df
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-23 14:05:41 -0400

    Add type-safe distance wrappers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/distance.kt
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.util
+
+interface Distance {
+    fun toMeters(): Meters
+}
+
+typealias RawDistance = Double
+
+private fun Number.toRawDistance(): RawDistance = this.toDouble()
+private fun Int.toRawDistance(): RawDistance = this.toDouble()
+private fun Double.toRawDistance(): RawDistance = this.toDouble()
+
+private const val INCHES_PER_FOOT = 12
+private const val FEET_PER_METER = 3.2808399
+private const val MM_PER_METER = 1000
+private const val CM_PER_METER = 100
+
+inline class Meters(val raw: RawDistance) : Distance {
+    override fun toMeters(): Meters {
+        return this
+    }
+}
+
+operator fun Meters.compareTo(other: Meters) = (this.raw).compareTo(other.raw)
+operator fun Meters.unaryMinus() = Meters(-this.raw)
+operator fun Meters.plus(other: Meters) = Meters(this.raw + other.raw)
+operator fun Meters.minus(other: Meters) = this + (-other)
+operator fun Meters.times(num: Double) = Meters(this.raw * num)
+operator fun Meters.times(num: Int) = this * (num.toDouble())
+operator fun Meters.div(other: Meters) = (this.raw) / (other.raw)
+operator fun Meters.div(other: Double) = Meters(this.raw / other)
+operator fun Meters.div(other: Int) = this / (other.toDouble())
+
+inline class Millimeters(val raw: RawDistance) : Distance {
+    override fun toMeters(): Meters {
+        return Meters(raw / MM_PER_METER)
+    }
+}
+
+fun Millimeters(raw: Int) = Millimeters(raw.toRawDistance())
+
+inline class Centimeters(val raw: RawDistance) : Distance {
+    override fun toMeters(): Meters {
+        return Meters(raw / CM_PER_METER)
+    }
+}
+
+fun Centimeters(raw: Int) = Centimeters(raw.toRawDistance())
+
+inline class Feet(val raw: RawDistance) : Distance {
+    override fun toMeters(): Meters {
+        return Meters(raw / FEET_PER_METER)
+    }
+}
+
+fun Feet(raw: Int) = Feet(raw.toRawDistance())
+
+inline class Inches(val raw: RawDistance) : Distance {
+    override fun toMeters(): Meters {
+        return Meters(raw / (INCHES_PER_FOOT * FEET_PER_METER))
+    }
+}
+
+fun Inches(raw: Int) = Inches(raw.toRawDistance())
+
+operator fun Distance.compareTo(other: Distance) = (this.toMeters()).compareTo(other.toMeters())
+operator fun Distance.unaryMinus() = -this.toMeters()
+operator fun Distance.plus(other: Distance) = this.toMeters() + other.toMeters()
+operator fun Distance.minus(other: Distance) = this.toMeters() - other.toMeters()
+operator fun Distance.times(other: Double) = this.toMeters() * other
+operator fun Distance.times(other: Int) = this.toMeters() * other
+operator fun Distance.div(other: Distance) = this.toMeters() / other.toMeters()
+operator fun Distance.div(other: Double) = this.toMeters() / other
+operator fun Distance.div(other: Int) = this.toMeters() / other
