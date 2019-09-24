c47fb2edbfd00c92138754dad2a135752f41d306
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-24 10:44:57 -0400

    Add YellowJacket motor interface

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/YellowJacket5202_0002_0019.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
+import com.qualcomm.robotcore.hardware.configuration.annotations.MotorType;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Rotation;
+
+@MotorType(ticksPerRev = 537.6, gearing = 19.2, maxRPM = 312, orientation = Rotation.CW)
+@DeviceProperties(name = "YellowJacket 5202-0002-0019", xmlTag = "YellowJacket5019_0002_0019", builtIn = false)
+public interface YellowJacket5202_0002_0019 {}
