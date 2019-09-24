87a0f751855383c3a0528b090302ce873ea81f8b
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-24 10:03:18 -0400

    Add InternalIMU class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/imu.kt
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.hardware
+
+import com.qualcomm.hardware.bosch.BNO055IMU
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation
+import org.firstinspires.ftc.teamcode.util.AbsoluteRadians
+import org.openftc.revextensions2.ExpansionHubEx
+
+private typealias RawIMU = BNO055IMU
+
+class InternalIMU {
+    companion object {
+        fun makeOptimized(hub: ExpansionHubEx) = InternalIMU(LynxOptimizedI2cFactory.createLynxEmbeddedImu(hub.standardModule, 0))
+    }
+
+    private val imu: RawIMU
+    private var angles: Orientation
+
+    public constructor(rawIMU: RawIMU) {
+        imu = rawIMU
+
+        val parameters = BNO055IMU.Parameters()
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC
+        parameters.loggingEnabled = true
+        parameters.loggingTag = "IMU"
+
+        imu.initialize(parameters)
+
+        angles = Orientation()
+    }
+
+    public fun update() {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS)
+    }
+
+    public fun heading() = AbsoluteRadians(angles.firstAngle)
+    public fun roll() = AbsoluteRadians(angles.secondAngle)
+    public fun pitch() = AbsoluteRadians(angles.thirdAngle)
+}
