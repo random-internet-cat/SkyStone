718895bb723d957e0701e44c82c4da1dec0d25ac
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-24 16:20:11 -0400

    Add Revolutions type-safe wrapper

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/revolutions.kt
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.util
+
+private typealias RawRevolutions = Double
+
+inline class Revolutions(val raw: RawRevolutions)
+
+inline operator fun Revolutions.compareTo(other: Revolutions) = (this.raw).compareTo(other.raw)
+inline operator fun Revolutions.unaryMinus() = Revolutions(-this.raw)
+inline operator fun Revolutions.plus(other: Revolutions) = Revolutions(this.raw + other.raw)
+inline operator fun Revolutions.minus(other: Revolutions) = Revolutions(this.raw - other.raw)
+inline operator fun Revolutions.times(num: Double) = Revolutions(this.raw * num)
+inline operator fun Revolutions.div(other: Revolutions) = this.raw / other.raw
+inline operator fun Revolutions.div(num: Double) = Revolutions(this.raw / num)
+
+inline operator fun Double.times(rev: Revolutions) = rev * this
