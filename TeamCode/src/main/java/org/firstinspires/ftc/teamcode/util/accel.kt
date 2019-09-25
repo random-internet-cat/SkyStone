996c8fce6603e4b9343f312cffd621e9e403a1c1
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-25 14:33:56 -0400

    Add acceleration and jerk units

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/accel.kt
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.util
+
+typealias RawAcceleration = Double
+
+interface Acceleration {
+    fun toMetersPerSecondSquared(): MetersPerSecondSquared
+}
+
+inline class MetersPerSecondSquared(val raw: RawAcceleration) : Acceleration {
+    inline override fun toMetersPerSecondSquared(): MetersPerSecondSquared {
+        return this
+    }
+}
+
+inline fun MetersPerSecondSquared(accel: MetersPerSecondSquared) = accel
+fun MetersPerSecondSquared(accel: Acceleration) = accel.toMetersPerSecondSquared()
+
+inline operator fun MetersPerSecond.div(time: Seconds) = MetersPerSecondSquared(this.raw / time.raw)
+
+inline operator fun MetersPerSecondSquared.compareTo(other: MetersPerSecondSquared) = (this.raw).compareTo(other.raw)
+inline operator fun MetersPerSecondSquared.unaryMinus() = MetersPerSecondSquared(-this.raw)
+inline operator fun MetersPerSecondSquared.plus(other: MetersPerSecondSquared) = MetersPerSecondSquared(this.raw + other.raw)
+inline operator fun MetersPerSecondSquared.minus(other: MetersPerSecondSquared) = MetersPerSecondSquared(this.raw - other.raw)
+inline operator fun MetersPerSecondSquared.times(time: Seconds) = MetersPerSecond(this.raw * time.raw)
+inline operator fun MetersPerSecondSquared.times(num: Double) = MetersPerSecondSquared(this.raw * num)
+inline operator fun MetersPerSecondSquared.div(num: Double) = MetersPerSecondSquared(this.raw / num)
+
+inline operator fun Seconds.times(time: MetersPerSecondSquared) = time * this
+inline operator fun Double.times(accel: MetersPerSecondSquared) = accel * this
+
+data class VelocityPerSecond(val velocity: Velocity, val time: Seconds) : Acceleration {
+    inline override fun toMetersPerSecondSquared(): MetersPerSecondSquared {
+        return velocity.toMetersPerSecond() / time
+    }
+}
+
+operator fun Velocity.div(time: Seconds) = VelocityPerSecond(this, time)
+
+data class VelocityPerTime(val velocity: Velocity, val time: Time) : Acceleration {
+    inline override fun toMetersPerSecondSquared(): MetersPerSecondSquared {
+        return velocity.toMetersPerSecond() / time.toSeconds()
+    }
+}
+
+operator fun Velocity.div(time: Time) = VelocityPerTime(this, time)
+
+operator fun Acceleration.compareTo(other: Acceleration) = (this.toMetersPerSecondSquared()).compareTo(other.toMetersPerSecondSquared())
+operator fun Acceleration.unaryMinus() = -this.toMetersPerSecondSquared()
+operator fun Acceleration.plus(other: Acceleration) = this.toMetersPerSecondSquared() + other.toMetersPerSecondSquared()
+operator fun Acceleration.minus(other: Acceleration) = this.toMetersPerSecondSquared() - other.toMetersPerSecondSquared()
+operator fun Acceleration.times(time: Time) = this.toMetersPerSecondSquared() * time.toSeconds()
+operator fun Acceleration.times(num: Double) = this.toMetersPerSecondSquared() * num
+operator fun Acceleration.div(num: Double) = this.toMetersPerSecondSquared() / num
+
+operator fun Time.times(accel: Acceleration) = this.toSeconds() * accel.toMetersPerSecondSquared()
+operator fun Double.times(accel: Acceleration) = this * accel.toMetersPerSecondSquared()
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/jerk.kt
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.util
+
+typealias RawJerk = Double
+
+interface Jerk {
+    fun toMetersPerSecondCubed(): MetersPerSecondCubed
+}
+
+inline class MetersPerSecondCubed(val raw: RawJerk) : Jerk {
+    inline override fun toMetersPerSecondCubed(): MetersPerSecondCubed {
+        return this
+    }
+}
+
+inline fun MetersPerSecondCubed(accel: MetersPerSecondCubed) = accel
+fun MetersPerSecondCubed(accel: Jerk) = accel.toMetersPerSecondCubed()
+
+inline operator fun MetersPerSecondSquared.div(time: Seconds) = MetersPerSecondCubed(this.raw / time.raw)
+
+inline operator fun MetersPerSecondCubed.compareTo(other: MetersPerSecondCubed) = (this.raw).compareTo(other.raw)
+inline operator fun MetersPerSecondCubed.unaryMinus() = MetersPerSecondCubed(-this.raw)
+inline operator fun MetersPerSecondCubed.plus(other: MetersPerSecondCubed) = MetersPerSecondCubed(this.raw + other.raw)
+inline operator fun MetersPerSecondCubed.minus(other: MetersPerSecondCubed) = MetersPerSecondCubed(this.raw - other.raw)
+inline operator fun MetersPerSecondCubed.times(time: Seconds) = MetersPerSecondSquared(this.raw * time.raw)
+inline operator fun MetersPerSecondCubed.times(num: Double) = MetersPerSecondCubed(this.raw * num)
+inline operator fun MetersPerSecondCubed.div(num: Double) = MetersPerSecondCubed(this.raw / num)
+
+inline operator fun Seconds.times(time: MetersPerSecondCubed) = time * this
+inline operator fun Double.times(jerk: MetersPerSecondCubed) = jerk * this
+
+data class AccelerationPerSecond(val distance: Acceleration, val time: Seconds) : Jerk {
+    inline override fun toMetersPerSecondCubed(): MetersPerSecondCubed {
+        return distance.toMetersPerSecondSquared() / time
+    }
+}
+
+operator fun Acceleration.div(time: Seconds) = AccelerationPerSecond(this, time)
+
+data class AccelerationPerTime(val distance: Acceleration, val time: Time) : Jerk {
+    inline override fun toMetersPerSecondCubed(): MetersPerSecondCubed {
+        return distance.toMetersPerSecondSquared() / time.toSeconds()
+    }
+}
+
+operator fun Acceleration.div(time: Time) = AccelerationPerTime(this, time)
+
+operator fun Jerk.compareTo(other: Jerk) = (this.toMetersPerSecondCubed()).compareTo(other.toMetersPerSecondCubed())
+operator fun Jerk.unaryMinus() = -this.toMetersPerSecondCubed()
+operator fun Jerk.plus(other: Jerk) = this.toMetersPerSecondCubed() + other.toMetersPerSecondCubed()
+operator fun Jerk.minus(other: Jerk) = this.toMetersPerSecondCubed() - other.toMetersPerSecondCubed()
+operator fun Jerk.times(time: Time) = this.toMetersPerSecondCubed() * time.toSeconds()
+operator fun Jerk.times(num: Double) = this.toMetersPerSecondCubed() * num
+operator fun Jerk.div(num: Double) = this.toMetersPerSecondCubed() / num
+
+operator fun Time.times(jerk: Jerk) = this.toSeconds() * jerk.toMetersPerSecondCubed()
+operator fun Double.times(jerk: Jerk) = this * jerk.toMetersPerSecondCubed()
