7aa7604dce1caa03f18f3451d054646e7545704d
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-26 09:55:39 -0400

    Add TankConstraints pseudo-constructors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/drive_constraints.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/drive_constraints.kt
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.util.roadrunner
 
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
+import com.acmerobotics.roadrunner.trajectory.constraints.TankConstraints
 import org.firstinspires.ftc.teamcode.util.units.*
 
 inline fun DriveConstraints(maxVel: RRVelocity, maxAccel: RRAcceleration, maxJerk: RRJerk, maxAngVel: RRAngularVelocity, maxAngAccel: RRAngularAcceleration, maxAngJerk: RRAngularJerk) = DriveConstraints(maxVel.roadrunner().raw, maxAccel.roadrunner().raw, maxJerk.roadrunner().raw, maxAngVel.roadrunner().raw, maxAngAccel.roadrunner().raw, maxAngJerk.roadrunner().raw)
@@ -8,3 +9,6 @@ fun DriveConstraints(maxVel: Velocity, maxAccel: Acceleration, maxJerk: Jerk, ma
 
 inline fun DriveConstraints(maxVel: RRVelocity, maxAccel: RRAcceleration, maxAngVel: RRAngularVelocity, maxAngAccel: RRAngularAcceleration) = DriveConstraints(maxVel.roadrunner().raw, maxAccel.roadrunner().raw, 0.0, maxAngVel.roadrunner().raw, maxAngAccel.roadrunner().raw, 0.0)
 fun DriveConstraints(maxVel: Velocity, maxAccel: Acceleration, maxAngVel: AngularVelocity, maxAngAccel: AngularAcceleration) = DriveConstraints(maxVel.roadrunner(), maxAccel.roadrunner(), maxAngVel.roadrunner(), maxAngAccel.roadrunner())
+
+fun TankConstraints(baseConstraints: DriveConstraints, trackWidth: RRDistance) = TankConstraints(baseConstraints, trackWidth.roadrunner().raw)
+fun TankConstraints(baseConstraints: DriveConstraints, trackWidth: Distance) = TankConstraints(baseConstraints, trackWidth.roadrunner())
