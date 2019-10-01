b330f46ce629a2e0096d1a08af305f52d9d04870
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-01 11:03:05 -0400

    Add motor PID util functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/motor.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/motor.kt
@@ -1,8 +1,20 @@
 package org.firstinspires.ftc.teamcode.util
 
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.PIDFCoefficients
 
 fun DcMotor.resetEncoder() {
     this.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER)
     this.setMode(DcMotor.RunMode.RUN_USING_ENCODER)
+}
+
+fun DcMotorEx.setAllPID(p: Double, i: Double, d: Double) {
+    setPID(DcMotor.RunMode.RUN_USING_ENCODER, p, i, d)
+    setPID(DcMotor.RunMode.RUN_TO_POSITION, p, i, d)
+}
+
+fun DcMotorEx.setPID(runmode: DcMotor.RunMode, p: Double, i: Double, d: Double) {
+    val f = getPIDFCoefficients(runmode).f
+    setPIDFCoefficients(runmode, PIDFCoefficients(p, i, d, f))
 }
