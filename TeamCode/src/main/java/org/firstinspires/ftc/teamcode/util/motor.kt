96199e30617c9cfbcd47c9b14d756bf96a11f277
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-03 10:30:48 -0400

    Remove DcMotorEx.setAllPID
    Doesn't make sense because RUN_TO_POSITION uses the RUN_USING_ENCODER logic internally

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/motor.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/motor.kt
@@ -10,11 +10,6 @@ fun DcMotor.resetEncoder() {
     this.setMode(DcMotor.RunMode.RUN_USING_ENCODER)
 }
 
-fun DcMotorEx.setAllPID(p: Double, i: Double, d: Double) {
-    setPID(DcMotor.RunMode.RUN_USING_ENCODER, p, i, d)
-    setPID(DcMotor.RunMode.RUN_TO_POSITION, p, i, d)
-}
-
 fun DcMotorEx.setPID(runmode: DcMotor.RunMode, p: Double, i: Double, d: Double) {
     val f = getPIDFCoefficients(runmode).f
     setPIDFCoefficients(runmode, PIDFCoefficients(p, i, d, f))
