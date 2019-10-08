b4a4bc9d6edd5e4a60e43693949addac3abcb5ea
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-08 10:20:40 -0400

    Add PIDFController psuedo-constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/pid.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/pid.kt
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.util.roadrunner
 
 import com.acmerobotics.roadrunner.control.PIDCoefficients
+import com.acmerobotics.roadrunner.control.PIDFController
+import com.acmerobotics.roadrunner.util.NanoClock
 import com.qualcomm.robotcore.hardware.PIDFCoefficients
+import org.firstinspires.ftc.teamcode.util.DcMotorFeedforward
 
 fun PIDCoefficients(pidf: PIDFCoefficients) = PIDCoefficients(pidf.p, pidf.i, pidf.d)
+
+fun PIDFController(pid: PIDCoefficients, feedforward: DcMotorFeedforward, kF: (Double) -> Double = { 0.0 }, clock: NanoClock = NanoClock.system()) = PIDFController(pid, feedforward.kV, feedforward.kA, feedforward.kStatic, kF, clock)
