f23e9d554d142cc2b1aa9661227dcf380b1c4490
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-08 14:46:19 -0400

    For RRDriveBases, take PID constants as parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/tank/TankDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/tank/TankDrive.kt
@@ -4,8 +4,11 @@ import com.acmerobotics.roadrunner.control.PIDCoefficients
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
 import org.firstinspires.ftc.teamcode.hardware.drive.BaseDriveEx
+import org.firstinspires.ftc.teamcode.hardware.drive.tank.TankDriveConstants.AXIAL_PID
 import org.firstinspires.ftc.teamcode.hardware.drive.tank.TankDriveConstants.BASE_CONSTRAINTS
+import org.firstinspires.ftc.teamcode.hardware.drive.tank.TankDriveConstants.CROSS_TRACK_PID
 import org.firstinspires.ftc.teamcode.hardware.drive.tank.TankDriveConstants.FEEDFORWARD
+import org.firstinspires.ftc.teamcode.hardware.drive.tank.TankDriveConstants.HEADING_PID
 import org.firstinspires.ftc.teamcode.util.roadrunner.PIDCoefficients
 import org.firstinspires.ftc.teamcode.util.roadrunner.roadrunner
 import org.firstinspires.ftc.teamcode.util.units.Meters
@@ -16,7 +19,7 @@ import org.firstinspires.ftc.teamcode.util.setPID
 
 class TankDrive(private val imu: InternalIMU, frontLeft: DcMotorEx, frontRight: DcMotorEx, backLeft: DcMotorEx, backRight: DcMotorEx) : BaseDriveEx(frontLeft, frontRight, backLeft, backRight) {
     private val roadrunnerValue by lazy {
-        object : RRTankDriveBase(TankDrivetrain(trackWidth = TRACK_WIDTH), FEEDFORWARD, BASE_CONSTRAINTS) {
+        object : RRTankDriveBase(TankDrivetrain(trackWidth = TRACK_WIDTH), TankDrivePID(axialPID = AXIAL_PID, headingPID = HEADING_PID, crossTrackPID = CROSS_TRACK_PID), FEEDFORWARD, BASE_CONSTRAINTS) {
             override fun getPIDCoefficients(runMode: DcMotor.RunMode): PIDCoefficients {
                 return PIDCoefficients(motors()[0].getPIDFCoefficients(runMode))
             }
