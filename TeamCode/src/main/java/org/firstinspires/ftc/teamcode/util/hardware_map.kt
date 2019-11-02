4f00b33af5b66430686d9119a9f4d13919cf6180
Jason Cobb <jason.e.cobb@gmail.com>
2019-11-02 12:52:29 -0400

    Add getHub helper function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/hardware_map.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/hardware_map.kt
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.util
 
 import com.qualcomm.robotcore.hardware.*
+import org.openftc.revextensions2.ExpansionHubEx
 
+fun HardwareMap.getHub(key: String = "hub") = get(ExpansionHubEx::class.java, key)
 fun HardwareMap.getMotor(key: String): DcMotor = get(DcMotor::class.java, key)
 fun HardwareMap.getMotorEx(key: String): DcMotorEx = get(DcMotorEx::class.java, key)
 fun HardwareMap.getCRServo(key: String): CRServo = get(CRServo::class.java, key)
