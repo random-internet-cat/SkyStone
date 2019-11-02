68a54c6fe7ec532540efbed3bb70768c61cacd2e
Jason Cobb <jason.e.cobb@gmail.com>
2019-11-02 13:18:33 -0400

    Add typealiases for Prototype hardware classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/prototype.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/prototype.kt
@@ -4,7 +4,10 @@ import org.firstinspires.ftc.teamcode.hardware.arm.PrototypeArm
 import org.firstinspires.ftc.teamcode.hardware.drive.tank.TankDrive
 import org.firstinspires.ftc.teamcode.intake.Intake
 
-class PrototypeHardware(private val arm: PrototypeArm, private val intake: Intake, private val drive: TankDrive) {
+typealias PrototypeIntake = Intake
+typealias PrototypeDrive = TankDrive
+
+class PrototypeHardware(private val arm: PrototypeArm, private val intake: PrototypeIntake, private val drive: PrototypeDrive) {
     fun arm() = arm
     fun intake() = intake
     fun drive() = drive
