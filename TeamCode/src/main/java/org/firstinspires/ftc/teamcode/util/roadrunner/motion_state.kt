32512c3747a8c6feb4851fcd8134494b4413e938
Jason Cobb <jason.e.cobb@gmail.com>
2019-11-09 12:42:27 -0500

    Add MotionState pseudo-constructor for linear position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/motion_state.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/motion_state.kt
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode.util.roadrunner
 
-import org.firstinspires.ftc.teamcode.util.units.AnglePoint
 import com.acmerobotics.roadrunner.profile.MotionState
-import org.firstinspires.ftc.teamcode.util.units.AngularAcceleration
-import org.firstinspires.ftc.teamcode.util.units.AngularJerk
-import org.firstinspires.ftc.teamcode.util.units.AngularVelocity
+import org.firstinspires.ftc.teamcode.util.units.*
+
+fun MotionState(position: RRDistance, vel: RRVelocity, accel: RRAcceleration, jerk: RRJerk) = MotionState(position.roadrunner().raw, vel.roadrunner().raw, accel.roadrunner().raw, jerk.roadrunner().raw)
+fun MotionState(position: Distance, vel: Velocity, accel: Acceleration, jerk: Jerk) = MotionState(position.roadrunner(), vel.roadrunner(), accel.roadrunner(), jerk.roadrunner())
 
 fun MotionState(position: RRAnglePoint, vel: RRAngularVelocity, accel: RRAngularAcceleration, jerk: RRAngularJerk) = MotionState(position.roadrunner().raw, vel.roadrunner().raw, accel.roadrunner().raw, jerk.roadrunner().raw)
 fun MotionState(position: AnglePoint, vel: AngularVelocity, accel: AngularAcceleration, jerk: AngularJerk) = MotionState(position.roadrunner(), vel.roadrunner(), accel.roadrunner(), jerk.roadrunner())
