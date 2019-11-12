3537647d69a80cf5ceda279da173a1122cf9bbad
Jason Cobb <jason.e.cobb@gmail.com>
2019-11-12 09:56:51 -0500

    Add EncoderTicksPerSecond multiplication

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/units/encoder.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/units/encoder.kt
@@ -39,3 +39,9 @@ inline operator fun Int.times(ticks: EncoderTicksPerSecond) = EncoderTicksPerSec
 
 inline operator fun EncoderTicks.div(time: Seconds) = EncoderTicksPerSecond(this.raw.toDouble() / time.raw)
 inline operator fun EncoderTicks.div(time: Time) = this / (time.toSeconds())
+
+inline operator fun EncoderTicksPerSecond.times(time: Seconds) = EncoderTicks((this.raw * time.raw).toInt())
+inline operator fun EncoderTicksPerSecond.times(time: Time) = this * time.toSeconds()
+
+inline operator fun Seconds.times(rate: EncoderTicksPerSecond) = rate * this
+inline operator fun Time.times(rate: EncoderTicksPerSecond) = (this.toSeconds()) * rate
