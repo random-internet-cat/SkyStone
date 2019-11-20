add746632f9d68ee8190f4e9a4edfc28d91d8513
Jason Cobb <jason.e.cobb@gmail.com>
2019-11-20 13:34:20 -0500

    Explicitly specify achievableMaxRPMFraction in custom motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/motors/YellowJacket5202_0002_0027.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/motors/YellowJacket5202_0002_0027.java
@@ -5,6 +5,6 @@ import com.qualcomm.robotcore.hardware.configuration.annotations.MotorType;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Rotation;
 
-@MotorType(ticksPerRev = 753.2, gearing = 26.9, maxRPM = 223, orientation = Rotation.CW)
+@MotorType(ticksPerRev = 753.2, gearing = 26.9, maxRPM = 223, achieveableMaxRPMFraction = 0.85, orientation = Rotation.CW)
 @DeviceProperties(name = "YellowJacket 5202-0002-0027", xmlTag = "YellowJacket5019_0002_0027", builtIn = false)
 public interface YellowJacket5202_0002_0027 {}
