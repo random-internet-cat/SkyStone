1ac6cb578d156927236095d7329d35ac67eb9d31
Jason Cobb <jason.e.cobb@gmail.com>
2019-11-21 10:04:53 -0500

    Add BaseFoundationMover

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/foundation_mover/foundation_mover.kt
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.hardware.foundation_mover
+
+import com.acmerobotics.dashboard.config.Config
+import com.qualcomm.robotcore.hardware.Servo
+
+typealias FoundationMoverPosition = Double
+
+interface FoundationMoverConfig {
+    val releasePosition: FoundationMoverPosition
+    val grabPosition: FoundationMoverPosition
+}
+
+open class BaseFoundationMover(val servos: List<Servo>, val config: FoundationMoverConfig) {
+    private fun setPosition(position: FoundationMoverPosition) {
+        servos.forEach { it.position = position }
+    }
+
+    fun grab() = setPosition(config.grabPosition)
+    fun release() = setPosition(config.releasePosition)
+
+    inline fun update() {}
+}
