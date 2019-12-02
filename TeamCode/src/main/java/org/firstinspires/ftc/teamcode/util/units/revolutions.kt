9cb2f9a299d13929af51db70715056407f98d339
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-02 10:12:11 -0500

    Add DistancePerRev unit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/units/revolutions.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/units/revolutions.kt
@@ -17,3 +17,51 @@ inline operator fun Revolutions.div(other: Revolutions) = this.raw / other.raw
 inline operator fun Revolutions.div(num: Double) = Revolutions(this.raw / num)
 
 inline operator fun Double.times(rev: Revolutions) = rev * this
+
+typealias RawDistancePerRev = Double
+
+interface DistancePerRev {
+    companion object {
+        fun zero() = MetersPerRev.zero()
+    }
+
+    fun toMetersPerRev(): MetersPerRev
+}
+
+inline class MetersPerRev(val raw: RawDistancePerRev) : DistancePerRev {
+    companion object {
+        fun zero() = MetersPerRev(0.0)
+    }
+
+    inline override fun toMetersPerRev(): MetersPerRev {
+        return this
+    }
+}
+
+fun MetersPerRev(raw: Int) = MetersPerRev(raw.toDouble())
+inline fun MetersPerRev(other: MetersPerRev) = other
+fun MetersPerRev(other: DistancePerRev) = other.toMetersPerRev()
+
+inline operator fun MetersPerRev.plus(other: MetersPerRev) = MetersPerRev(this.raw + other.raw)
+inline operator fun MetersPerRev.minus(other: MetersPerRev) = MetersPerRev(this.raw - other.raw)
+inline operator fun MetersPerRev.times(other: RawDistancePerRev) = MetersPerRev(this.raw * other)
+inline operator fun RawDistancePerRev.times(other: MetersPerRev) = MetersPerRev(this * other.raw)
+
+inline operator fun MetersPerRev.times(other: Revolutions) = Meters(this.raw * other.raw)
+inline operator fun Revolutions.times(other: MetersPerRev) = Meters(this.raw * other.raw)
+inline operator fun MetersPerRev.times(other: RevolutionsPerSecond) = MetersPerSecond(this.raw * other.raw)
+inline operator fun RevolutionsPerSecond.times(other: MetersPerRev) = MetersPerSecond(this.raw * other.raw)
+inline operator fun Meters.div(other: MetersPerRev) = Revolutions(this.raw / other.raw)
+inline operator fun Meters.div(other: Revolutions) = MetersPerRev((this.raw.toDouble()) / (other.raw.toDouble()))
+
+operator fun DistancePerRev.plus(other: DistancePerRev) = this.toMetersPerRev() + other.toMetersPerRev()
+operator fun DistancePerRev.minus(other: DistancePerRev) = this.toMetersPerRev() - other.toMetersPerRev()
+operator fun DistancePerRev.times(other: RawDistancePerRev) = this.toMetersPerRev() * other
+operator fun RawDistancePerRev.times(other: DistancePerRev) = this * other.toMetersPerRev()
+
+operator fun DistancePerRev.times(other: Revolutions) = this.toMetersPerRev() * other
+operator fun Revolutions.times(other: DistancePerRev) = this * other.toMetersPerRev()
+operator fun DistancePerRev.times(other: AngularVelocity) = this.toMetersPerRev() * other.toRevolutionsPerSecond()
+operator fun AngularVelocity.times(other: DistancePerRev) = this.toRevolutionsPerSecond() * other.toMetersPerRev()
+operator fun Distance.div(other: DistancePerRev) = this.toMeters() / other.toMetersPerRev()
+operator fun Distance.div(other: Revolutions) = this.toMeters() / other
