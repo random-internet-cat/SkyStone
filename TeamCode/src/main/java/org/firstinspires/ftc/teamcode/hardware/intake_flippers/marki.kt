92420b1afc471a723ad636f63ae0527af8e44896
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-04 08:07:16 -0500

    Add intake flippers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake_flippers/marki.kt
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.hardware.intake_flippers
+
+import com.acmerobotics.dashboard.config.Config
+import com.qualcomm.robotcore.hardware.Servo
+
+@Config
+class MarkIIntakeFlippers(val servos: List<Servo>) {
+    constructor(vararg servos: Servo) : this(servos.toList())
+
+    companion object {
+        @JvmField
+        public var RELEASE_POSITION = 0.5
+    }
+
+    fun release() {
+        servos.forEach { it.position = RELEASE_POSITION }
+    }
+
+    inline fun update() {}
+}
