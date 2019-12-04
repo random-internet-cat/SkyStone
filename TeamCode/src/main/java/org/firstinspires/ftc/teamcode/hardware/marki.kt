e924ad49d0f8516fb49bea753588ac4e30e34775
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-04 08:07:16 -0500

    Add intake flippers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/marki.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/marki.kt
@@ -4,14 +4,16 @@ import org.firstinspires.ftc.teamcode.hardware.arm.MarkIArm
 import org.firstinspires.ftc.teamcode.hardware.drive.mecanum.MecanumDrive
 import org.firstinspires.ftc.teamcode.hardware.foundation_mover.MarkIFoundationMover
 import org.firstinspires.ftc.teamcode.hardware.intake.MarkIIntake
+import org.firstinspires.ftc.teamcode.hardware.intake_flippers.MarkIIntakeFlippers
 
 typealias MarkIDrivetrain = MecanumDrive
 
-data class MarkIHardware(val drive: MarkIDrivetrain, val arm: MarkIArm, val intake: MarkIIntake, val foundationMover: MarkIFoundationMover) {
+data class MarkIHardware(val drive: MarkIDrivetrain, val arm: MarkIArm, val intake: MarkIIntake, val intakeFlippers: MarkIIntakeFlippers, val foundationMover: MarkIFoundationMover) {
     fun update() {
         drive.update()
         arm.update()
         intake.update()
+        intakeFlippers.update()
         foundationMover.update()
     }
 }
