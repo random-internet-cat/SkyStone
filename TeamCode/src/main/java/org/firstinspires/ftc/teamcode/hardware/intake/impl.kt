fbd41dd72a0080e1229ca1c5565b9ee01c2119e7
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-12 10:51:22 -0500

    Nuke all of the prototype classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake/impl.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake/impl.kt
@@ -2,10 +2,6 @@ package org.firstinspires.ftc.teamcode.hardware.intake
 
 import com.qualcomm.robotcore.hardware.DcMotor
 
-class PrototypeIntake(motors: List<DcMotor>) : FixedPowerIntake(1.0, motors) {
-    constructor(vararg motors: DcMotor) : this(motors.toList())
-}
-
 class MarkIIntake(motors: List<DcMotor>) : FixedPowerIntake(0.5, motors) {
     constructor(vararg motors: DcMotor) : this(motors.toList())
 }
