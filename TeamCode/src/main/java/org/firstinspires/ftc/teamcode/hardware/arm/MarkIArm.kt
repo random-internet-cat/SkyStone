f9a8ec3c3f8e59f8fe32a8737e3d455f0186472f
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-17 09:48:30 -0500

    Rewrite arm vertical to use State/manual movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/arm/MarkIArm.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/arm/MarkIArm.kt
@@ -4,10 +4,7 @@ import com.acmerobotics.dashboard.config.Config
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.util.*
-import org.firstinspires.ftc.teamcode.util.roadrunner.PIDCoefficients
-import org.firstinspires.ftc.teamcode.util.roadrunner.roadrunner
 import org.firstinspires.ftc.teamcode.util.units.*
-import kotlin.math.PI
 
 data class MarkIArm(val horizontal: HorizontalControl, val vertical: VerticalControl, val clamp: Clamp) {
     data class HorizontalControl(val motor: DcMotor) {
@@ -36,30 +33,143 @@ data class MarkIArm(val horizontal: HorizontalControl, val vertical: VerticalCon
         fun update() {}
     }
 
+    @Config("MarkIArm VerticalControl")
     data class VerticalControl(val motor: DcMotor) {
         companion object {
-            private const val MOTOR_POWER = 0.8
+            private const val MANUAL_MOTOR_POWER = 0.8
+            private const val AUTOMATIC_MOTOR_POWER = 0.6
+
+            @JvmField
+            public var _COLLECT_TICKS: Int = 0
+
+            @JvmField
+            public var _STAGE0_TICKS: Int = 0
+
+            @JvmField
+            public var _PER_STAGE_TICKS: Int = 0
+
+            private val COLLECT_POSITION get() = EncoderPosition(_COLLECT_TICKS)
+            private val STAGE0_POSITION get() = EncoderPosition(_STAGE0_TICKS)
+            private val PER_STAGE_TICKS get() = EncoderTicks(_PER_STAGE_TICKS)
         }
 
-        private fun power(power: Double) {
+        sealed class State {
+            companion object {
+                private const val MIN_BLOCK_HEIGHT = 0
+                private const val MAX_BLOCK_HEIGHT = 6
+            }
+
+            abstract val nextDown: State?
+            abstract val nextUp: State?
+            abstract val position: EncoderPosition
+
+            object CollectState : State() {
+                override val nextDown: State? get() = null
+                override val nextUp: State? get() = PlaceBlockState(MIN_BLOCK_HEIGHT)
+                override val position: EncoderPosition
+                    get() = COLLECT_POSITION
+            }
+
+            data class PlaceBlockState(val blockHeight: Int) : State() {
+                init {
+                    require(blockHeight >= MIN_BLOCK_HEIGHT && blockHeight <= MAX_BLOCK_HEIGHT)
+                }
+
+                override val nextDown: State?
+                    get() = if (blockHeight == MIN_BLOCK_HEIGHT) CollectState else PlaceBlockState(blockHeight - 1)
+
+                override val nextUp: State?
+                    get() = if (blockHeight == MAX_BLOCK_HEIGHT) null else PlaceBlockState(blockHeight + 1)
+
+                override val position: EncoderPosition
+                    get() = STAGE0_POSITION + blockHeight * PER_STAGE_TICKS
+            }
+        }
+
+        private var _state: State
+
+        public fun lastAutomaticState() = _state
+        public fun currentAutomaticState() = if (!isManual) _state else null
+
+        init {
+            motor.resetEncoder()
+            motor.targetPosition = 0
+            _state = State.CollectState
+        }
+
+        private var _mutableIsManual: Boolean = true
+
+        private val isManual get() = _mutableIsManual
+        private val isAutomatic get() = !isManual
+
+        private fun manuallyMoveWithPower(power: Double) {
+            markManual()
             motor.power = power
         }
 
-        private fun power(power: Int) = power(power.toDouble())
-
-        fun moveUp() {
-            power(MOTOR_POWER)
+        private fun markManual() {
+            _mutableIsManual = true
         }
 
-        fun moveDown() {
-            power(-MOTOR_POWER)
+        private fun markAutomatic() {
+            _mutableIsManual = false
+        }
+
+        fun manuallyMoveUp() {
+            manuallyMoveWithPower(MANUAL_MOTOR_POWER)
+        }
+
+        fun manuallyMoveDown() {
+            manuallyMoveWithPower(-MANUAL_MOTOR_POWER)
         }
 
         fun stop() {
-            power(0)
+            motor.power = 0.0
         }
 
-        fun update() {}
+        fun stopIfManual() {
+            if (isManual) stop()
+        }
+
+        private fun setMotorRunMode(runMode: DcMotor.RunMode) {
+            motor.mode = runMode
+        }
+
+        private fun moveToPosition(encoderPosition: EncoderPosition) {
+            motor.targetPosition = encoderPosition.raw
+            motor.power = AUTOMATIC_MOTOR_POWER
+            setMotorRunMode(DcMotor.RunMode.RUN_TO_POSITION)
+        }
+
+        private fun setCurrentState(state: State) {
+            markAutomatic()
+            _state = state
+        }
+
+        private fun moveToCurrentState() {
+            val currentState = currentAutomaticState()
+            require(currentState != null) { "Request to move to state in manual mode" }
+            moveToPosition(currentState.position)
+        }
+
+        private fun moveToCurrentStateIfAutomatic() {
+            if (isAutomatic) moveToCurrentState()
+        }
+
+        fun moveToState(state: State) {
+            setCurrentState(state)
+            moveToCurrentState()
+        }
+
+        fun isMovingToState(): Boolean {
+            check(isAutomatic)
+            check(motor.mode == DcMotor.RunMode.RUN_TO_POSITION)
+            return motor.isBusy
+        }
+
+        fun update() {
+            moveToCurrentStateIfAutomatic()
+        }
     }
 
     @Config
