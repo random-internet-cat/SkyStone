c7e1e9403512a33a3275cd8b3a0de8b190e0377b
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-21 14:20:29 -0500

    Fix DashboardUtil units

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/DashboardUtil.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/DashboardUtil.kt
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.canvas.Canvas
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.acmerobotics.roadrunner.path.Path
+import org.firstinspires.ftc.teamcode.util.units.Distance
 import org.firstinspires.ftc.teamcode.util.units.Inches
 import org.firstinspires.ftc.teamcode.util.units.div
 import kotlin.math.ceil
@@ -12,11 +13,11 @@ import kotlin.math.ceil
  * Set of helper functions for drawing Road Runner paths and trajectories on dashboard canvases.
  */
 object DashboardUtil {
-    private val DEFAULT_RESOLUTION = Inches(2.0).roadrunner() // distance units; presumed inches
-    private val ROBOT_RADIUS = Inches(9.0).roadrunner() // in
+    private val DEFAULT_RESOLUTION = Inches(2.0)
+    private val ROBOT_RADIUS = Inches(9.0)
 
     @JvmOverloads
-    fun drawSampledPath(canvas: Canvas, path: Path, resolution: RRDistance = DEFAULT_RESOLUTION) {
+    fun drawSampledPath(canvas: Canvas, path: Path, resolution: Distance = DEFAULT_RESOLUTION) {
         val samples = ceil(RRDistance(path.length()) / resolution).toInt()
         val xPoints = DoubleArray(samples)
         val yPoints = DoubleArray(samples)
@@ -24,19 +25,23 @@ object DashboardUtil {
         for (i in 0 until samples) {
             val displacement = i * dx
             val (x, y) = path[displacement]
-            xPoints[i] = x
-            yPoints[i] = y
+            xPoints[i] = Inches(RRDistance(x)).raw
+            yPoints[i] = Inches(RRDistance(y)).raw
         }
         canvas.strokePolyline(xPoints, yPoints)
     }
 
-    fun drawRobot(canvas: Canvas, pose: Pose2d) {
-        canvas.strokeCircle(pose.x, pose.y, ROBOT_RADIUS.roadrunner().raw)
-        val (x, y) = pose.headingVec() * (ROBOT_RADIUS.roadrunner().raw)
-        val x1 = pose.x + x / 2
-        val y1 = pose.y + y / 2
-        val x2 = pose.x + x
-        val y2 = pose.y + y
+    fun drawRobot(canvas: Canvas, rawPose: Pose2d) {
+        val poseX = Inches(RRDistance(rawPose.x)).raw
+        val poseY = Inches(RRDistance(rawPose.y)).raw
+        val robotRadius = Inches(ROBOT_RADIUS).raw
+
+        canvas.strokeCircle(poseX, poseY, robotRadius)
+        val (x, y) = rawPose.headingVec() * (robotRadius)
+        val x1 = poseX + x / 2
+        val y1 = poseY + y / 2
+        val x2 = poseX + x
+        val y2 = poseY + y
         canvas.strokeLine(x1, y1, x2, y2)
     }
 }
