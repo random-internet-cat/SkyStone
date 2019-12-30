8339ce9d7be184c84e8a01597daa708c967ce095
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-30 12:21:12 -0500

    Make AutoBase an abstract base and parameterize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoBase.java
@@ -1,121 +1,124 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.mecanum.RRMecanumDriveBase;
 import org.firstinspires.ftc.teamcode.hardware.MarkIHardware;
+import org.firstinspires.ftc.teamcode.hardware.foundation_mover.MarkIFoundationMover;
 import org.firstinspires.ftc.teamcode.hardware.provider.MarkIHardwareProvider;
 
 import static org.firstinspires.ftc.teamcode.util.RRUnits.degHeading;
 import static org.firstinspires.ftc.teamcode.util.RRUnits.inches;
 import static org.firstinspires.ftc.teamcode.util.RRUnits.inchesVector;
-import static org.firstinspires.ftc.teamcode.util.RRUnits.radHeading;
-import static org.firstinspires.ftc.teamcode.util.RRUnits.zeroDistance;
-import static org.firstinspires.ftc.teamcode.util.RRUnits.zeroHeading;
 
-@Autonomous
-public class AutoBase extends LinearOpMode {
+public abstract class AutoBase extends LinearOpMode {
     enum QuarryState {
-        CLOSE_TO_BRIDGES(inchesVector(-36, 33)),
-        MIDDLE(inchesVector(-43, 33)),
-        CLOSE_TO_WALL(inchesVector(-50, 33)),
+        CLOSE_TO_BRIDGES(inches(-36)),
+        MIDDLE(inches(-43)),
+        CLOSE_TO_WALL(inches(-50)),
         ;
 
-        private final Vector2d grabPosition;
+        private final double xPosition;
 
-        private QuarryState(Vector2d grabPosition) {
-            this.grabPosition = grabPosition;
+        private QuarryState(double xPosition) {
+            this.xPosition = xPosition;
         }
 
-        public Vector2d grabPosition() {
-            return grabPosition;
+        public double xPosition() {
+            return xPosition;
         }
     }
 
-    private QuarryState readQuarry() {
+    enum SkystoneRelativePos {
+        LEFT,
+        MIDDLE,
+        RIGHT
+        ;
+    }
+
+    protected final SkystoneRelativePos readQuarryRelative() {
         // TODO: actually figure out where stones are
-        return QuarryState.CLOSE_TO_BRIDGES;
+        return SkystoneRelativePos.RIGHT;
     }
 
-    private void checkInterrupted() throws InterruptedException {
+    protected abstract QuarryState mapQuarryState(SkystoneRelativePos relativePos);
+
+    private QuarryState readQuarryState() { return mapQuarryState(readQuarryRelative()); }
+
+    protected final void checkInterrupted() throws InterruptedException {
         if (isStopRequested() || Thread.currentThread().isInterrupted()) throw new InterruptedException();
     }
 
+    protected abstract Pose2d startPosition();
+
+    private void setupDrive(RRMecanumDriveBase drive) {
+        drive.setPoseEstimate(startPosition());
+    }
+
+    protected abstract double grabStoneYPos();
+    protected abstract double grabStoneHeading();
+
+    private void moveToGrabStoneInternal(RRMecanumDriveBase drive, QuarryState quarryState) {
+        drive.followTrajectorySync(drive.trajectoryBuilder()
+                                        .setReversed(false)
+                                        .splineTo(new Pose2d(
+                                            quarryState.xPosition(),
+                                            grabStoneYPos(),
+                                            grabStoneHeading()
+                                        )).build());
+    }
+
+    private void moveToGrabStone(RRMecanumDriveBase drive) {
+        moveToGrabStoneInternal(drive, readQuarryState());
+    }
+
+    private void grabFoundation(MarkIFoundationMover foundationMover) {
+        foundationMover.grab();
+        sleep(1500 /* ms */);
+    }
+
+    private void releaseFoundation(MarkIFoundationMover foundationMover) {
+        foundationMover.release();
+        sleep(1500 /* ms */);
+    }
+
+    protected abstract void moveToGrabFoundation(RRMecanumDriveBase drive);
+    protected abstract void moveFoundationToQuarry(RRMecanumDriveBase drive);
+    protected abstract void park(RRMecanumDriveBase drive);
+
     @Override
-    public void runOpMode() throws InterruptedException {
+    public final void runOpMode() throws InterruptedException {
         MarkIHardware hardware = MarkIHardwareProvider.makeHardware(hardwareMap);
         RRMecanumDriveBase drive = hardware.getDrive().roadrunner();
 
-        drive.setPoseEstimate(new Pose2d(inchesVector(-32.0, 60.0), degHeading(270)));
+        setupDrive(drive);
 
         waitForStart();
 
         checkInterrupted();
 
-        QuarryState quarryState = readQuarry();
-        // TODO: can this be null? if it can, implement error handling
-        if (quarryState == null) quarryState = QuarryState.CLOSE_TO_BRIDGES;
-
-        // Move to stone
-        drive.followTrajectorySync(drive.trajectoryBuilder()
-                .setReversed(false)
-                .splineTo(new Pose2d(
-                        quarryState.grabPosition(),
-                        degHeading(270)
-                )).build());
+        moveToGrabStone(drive);
 
         checkInterrupted();
 
-        // Move to center of field
-        drive.followTrajectorySync(drive.trajectoryBuilder()
-                .setReversed(true)
-                .splineTo(new Pose2d(inchesVector(12, 36), degHeading(180)))
-                .build());
+        moveToGrabFoundation(drive);
 
         checkInterrupted();
 
-        // Move to foundation
-        drive.followTrajectorySync(drive.trajectoryBuilder()
-                .splineTo(new Pose2d(inchesVector(48, 31), degHeading(90)))
-                .build()
-        );
+        grabFoundation(hardware.getFoundationMover());
 
         checkInterrupted();
 
-        // Grab foundation
-        hardware.getFoundationMover().grab();
-        sleep(1500);
+        moveFoundationToQuarry(drive);
 
         checkInterrupted();
 
-        // Spline to make foundation horizontal
-        drive.followTrajectorySync(drive.trajectoryBuilder()
-                .setReversed(false)
-                .splineTo(new Pose2d(inchesVector(30, 42), degHeading(180)))
-                .build());
+        releaseFoundation(hardware.getFoundationMover());
 
         checkInterrupted();
 
-        drive.followTrajectorySync(drive.trajectoryBuilder()
-                .setReversed(true)
-                .forward(inches(-16))
-                .build());
-
-        checkInterrupted();
-
-        // Release foundation
-        hardware.getFoundationMover().release();
-
-        checkInterrupted();
-
-        // Park
-        drive.followTrajectorySync(drive.trajectoryBuilder()
-                .setReversed(false)
-                .splineTo(new Pose2d(inchesVector(0, 36), degHeading(180)))
-                .build());
+        park(drive);
 
         checkInterrupted();
     }
