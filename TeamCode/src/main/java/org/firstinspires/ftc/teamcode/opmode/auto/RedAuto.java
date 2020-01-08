1f4de87c215eff046ee0429bce6b7cc2fbad0522
Jason Cobb <jason.e.cobb@gmail.com>
2020-01-08 10:44:51 -0500

    Add RedAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/RedAuto.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import java.util.Objects;
+
+import static org.firstinspires.ftc.teamcode.util.RRUnits.degHeading;
+
+@Autonomous
+@Config
+public class RedAuto extends SidedAutoBase {
+    RedAuto() {
+        super(SideYSign.POSITIVE);
+    }
+
+    @Override
+    protected QuarryState mapQuarryState(SkystoneRelativePos relativePos) {
+        Objects.requireNonNull(relativePos, "relativePos");
+
+        switch (relativePos) {
+            case RIGHT: return QuarryState.CLOSE_TO_BRIDGES;
+            case MIDDLE: return QuarryState.MIDDLE;
+            case LEFT: return QuarryState.CLOSE_TO_WALL;
+        }
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected double headingTowardsFoundationWall() {
+        return degHeading(0);
+    }
+
+    @Override
+    protected double headingTowardsHomeWall() {
+        return degHeading(270);
+    }
+}
