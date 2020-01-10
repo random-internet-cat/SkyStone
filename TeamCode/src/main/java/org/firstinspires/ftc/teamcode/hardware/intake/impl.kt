dcd431bf112979c3b56e57c2e19df0f2c6005faf
Jason Cobb <jason.e.cobb@gmail.com>
2020-01-10 10:59:08 -0500

    Add servos support for intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake/impl.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake/impl.kt
@@ -1,7 +1,45 @@
 package org.firstinspires.ftc.teamcode.hardware.intake
 
+import com.qualcomm.robotcore.hardware.CRServo
 import com.qualcomm.robotcore.hardware.DcMotor
 
-class MarkIIntake(motors: List<DcMotor>) : FixedPowerIntake(0.5, motors) {
-    constructor(vararg motors: DcMotor) : this(motors.toList())
+class MarkIIntake {
+    private val mainMotors: List<DcMotor>
+    private val servos: List<CRServo>
+
+    constructor(mainMotors: List<DcMotor>, servos: List<CRServo>) {
+        // Defensive copies
+        this.mainMotors = mainMotors.toList()
+        this.servos = servos.toList()
+    }
+
+    private fun powerMotors(newPower: Double) {
+        mainMotors.forEach { it.power = newPower }
+    }
+
+    private fun powerServos(newPower: Double) {
+        servos.forEach { it.power = newPower }
+    }
+
+    fun intake() {
+        powerMotors(MOTOR_POWER)
+        powerServos(SERVO_POWER)
+    }
+
+    fun outtake() {
+        powerMotors(-MOTOR_POWER)
+        powerServos(-SERVO_POWER)
+    }
+
+    fun stop() {
+        powerMotors(0.0)
+        powerServos(0.0)
+    }
+
+    fun update() {}
+
+    companion object {
+        private const val MOTOR_POWER = 0.5
+        private const val SERVO_POWER = 0.8
+    }
 }
