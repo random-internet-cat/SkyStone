7026084692ded172ea642acba90b7feeb825f3af
Jason Cobb <jason.e.cobb@gmail.com>
2020-01-16 16:13:26 -0500

    Update foundation move by adding two more states and tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/foundation_mover/marki.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/foundation_mover/marki.kt
@@ -7,12 +7,20 @@ import com.qualcomm.robotcore.hardware.Servo
 class MarkIFoundationMover(servos: List<Servo>) : BaseFoundationMover(servos, object : FoundationMoverConfig {
     override val grabPosition: FoundationMoverPosition get() = GRAB_POSITION
     override val releasePosition: FoundationMoverPosition get() = RELEASE_POSITION
+    override val collectStonePosition: FoundationMoverPosition get() = COLLECT_STONE_POSITION
+    override val stoneAboveGroundPosition: FoundationMoverPosition get() = STONE_ABOVE_GROUND_POSITION
 }) {
     constructor(vararg servos: Servo) : this(servos.toList())
 
     companion object {
         @JvmField
-        public var GRAB_POSITION = 0.15
+        public var GRAB_POSITION = 0.3
+
+        @JvmField
+        public var COLLECT_STONE_POSITION = 0.4
+
+        @JvmField
+        public var STONE_ABOVE_GROUND_POSITION = 0.55
 
         @JvmField
         public var RELEASE_POSITION = 1.0
