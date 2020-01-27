000c94883af4d6c86fc00c236b578a7709ca17a7
Jason Cobb <jason.e.cobb@gmail.com>
2020-01-27 13:38:40 -0500

    Add hardware testing/tuning opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/FoundationMoversOutOfTheWay.kt
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.opmode.auto
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.hardware.provider.makeMarkIHardware
+
+@Autonomous
+class FoundationMoversOutOfTheWay : LinearOpMode() {
+    override fun runOpMode() {
+        val hardware = makeMarkIHardware(hardwareMap)
+
+        waitForStart()
+
+        while (opModeIsActive()) {
+            hardware.foundationMover.moveBothToOutOfTheWay()
+        }
+    }
+}
