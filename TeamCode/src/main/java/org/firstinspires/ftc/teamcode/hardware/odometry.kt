a61b842f0494ca3d9cefed1150007354895b69d2
Jason Cobb <jason.e.cobb@gmail.com>
2020-02-06 17:32:10 -0500

    Move OdoPodDesc to file scope

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/odometry.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/odometry.kt
@@ -7,3 +7,7 @@ object MarkIOdomtetryConstants {
     public var PARALLEL_DISTANCE_IN: Double = 14.5
     public var BACK_DISTANCE_IN: Double = -2.5
 }
+
+data class OdoPodDesc(val pose: Pose2d, val motor: DcMotor) {
+    constructor(pose: RobotPosition, motor: DcMotor) : this(pose.roadrunner(), motor)
+}
