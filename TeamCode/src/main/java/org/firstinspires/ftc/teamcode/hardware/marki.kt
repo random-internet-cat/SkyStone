f56e09b5803dda68d47a5046ef6d2e457cd4e9d5
Jason Cobb <jason.e.cobb@gmail.com>
2020-02-08 10:01:51 -0500

    Remove intake flippers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/marki.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/marki.kt
@@ -4,17 +4,15 @@ import org.firstinspires.ftc.teamcode.hardware.arm.MarkIArm
 import org.firstinspires.ftc.teamcode.hardware.drive.mecanum.MecanumDrive
 import org.firstinspires.ftc.teamcode.hardware.foundation_mover.MarkIFoundationMover
 import org.firstinspires.ftc.teamcode.hardware.intake.MarkIIntake
-import org.firstinspires.ftc.teamcode.hardware.intake_flippers.MarkIIntakeFlippers
 import org.firstinspires.ftc.teamcode.hardware.auto_claw.MarkIAutoClaws
 
 typealias MarkIDrivetrain = MecanumDrive
 
-data class MarkIHardware(val drive: MarkIDrivetrain, val arm: MarkIArm, val intake: MarkIIntake, val intakeFlippers: MarkIIntakeFlippers, val foundationMover: MarkIFoundationMover, val autoClaws: MarkIAutoClaws) {
+data class MarkIHardware(val drive: MarkIDrivetrain, val arm: MarkIArm, val intake: MarkIIntake, val foundationMover: MarkIFoundationMover, val autoClaws: MarkIAutoClaws) {
     fun update(updateDashboard: Boolean = true) {
         drive.update(updateDashboard = updateDashboard)
         arm.update()
         intake.update()
-        intakeFlippers.update()
         foundationMover.update()
         autoClaws.update()
     }
