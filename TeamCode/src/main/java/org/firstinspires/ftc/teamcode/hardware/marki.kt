4fcd30ee9d99c9c1b5fb2a75a535e4a9631cf18c
Jason Cobb <jason.e.cobb@gmail.com>
2020-02-15 12:41:02 -0500

    Remove auto claws

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/marki.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/marki.kt
@@ -4,16 +4,14 @@ import org.firstinspires.ftc.teamcode.hardware.arm.MarkIArm
 import org.firstinspires.ftc.teamcode.hardware.drive.mecanum.MecanumDrive
 import org.firstinspires.ftc.teamcode.hardware.foundation_mover.MarkIFoundationMover
 import org.firstinspires.ftc.teamcode.hardware.intake.MarkIIntake
-import org.firstinspires.ftc.teamcode.hardware.auto_claw.MarkIAutoClaws
 
 typealias MarkIDrivetrain = MecanumDrive
 
-data class MarkIHardware(val drive: MarkIDrivetrain, val arm: MarkIArm, val intake: MarkIIntake, val foundationMover: MarkIFoundationMover, val autoClaws: MarkIAutoClaws) {
+data class MarkIHardware(val drive: MarkIDrivetrain, val arm: MarkIArm, val intake: MarkIIntake, val foundationMover: MarkIFoundationMover) {
     fun update(updateDashboard: Boolean = true) {
         drive.update(updateDashboard = updateDashboard)
         arm.update()
         intake.update()
         foundationMover.update()
-        autoClaws.update()
     }
 }
