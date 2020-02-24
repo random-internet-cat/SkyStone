d52cc1cf98c0c60423137ebf2cc75a80f6675b0e
Lukas Vantilburg <lukasvantilburg@gmail.com>
2020-02-24 18:26:06 -0500

    Boosted feedfoward on horizontal and vertical

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/provider/marki.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/provider/marki.kt
@@ -59,7 +59,7 @@ object MarkIHardwareProvider {
     @JvmStatic
     fun makeArmHorizontal(hardwareMap: HardwareMap): MarkIArm.HorizontalControl {
         val motor = hardwareMap.getMotorEx("horizontal")
-        return MarkIArm.HorizontalControl(motor).apply { setPIDF(PIDFCoefficients(1.17, 0.05, 0.0, 12.0)) }
+        return MarkIArm.HorizontalControl(motor).apply { setPIDF(PIDFCoefficients(1.17, 0.05, 0.0, 15.0)) }
     }
 
     @JvmStatic
@@ -69,7 +69,7 @@ object MarkIHardwareProvider {
         motor.brakeOnZeroPower()
         motor.enableEncoder()
         return MarkIArm.VerticalControl(motor).apply {
-            setPIDF(PIDFCoefficients(.11, 0.01, 0.0,12.0))
+            setPIDF(PIDFCoefficients(.11, 0.01, 0.0,16.0))
         }
     }
 
